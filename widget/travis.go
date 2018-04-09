5e9f4116f1dcbdc031f81c8414853ee64dd44986
Ben Selby <ben.selby@emisgroupplc.com>
2018-04-09 08:17:13 +0100

    Paint failed travis builds accordingly
    https://github.com/benmatselby/precis/projects/1#card-8783344

--- widget/travis.go
+++ widget/travis.go
@@ -19,20 +19,33 @@ func Travis(token string, owner string) *termui.Table {
 	rows := [][]string{
 		{"repo", "state", "finished"},
 	}
+	sadRows := []int{}
+	happyRows := []int{}
+
 	for _, repo := range repos {
 
+		// Trying to remove the items that are not really running in Travis CI
+		// Assume there is a better way to do this?
 		if repo.LastBuildState == "" {
 			continue
 		}
+
 		branch, _, err := client.Branches.GetFromSlug(repo.Slug, "master")
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		rows = append(rows, []string{repo.Slug, branch.State, branch.FinishedAt})
+
+		if branch.State == "failed" {
+			sadRows = append(sadRows, len(rows)-1)
+		} else {
+			happyRows = append(happyRows, len(rows)-1)
+		}
+
 	}
 
 	w := termui.NewTable()
-
 	w.Rows = rows
 	w.FgColor = termui.ColorWhite
 	w.BgColor = termui.ColorDefault
@@ -42,6 +55,14 @@ func Travis(token string, owner string) *termui.Table {
 
 	w.Analysis()
 	w.SetSize()
+
+	for _, line := range sadRows {
+		w.FgColors[line] = termui.ColorRed
+	}
+
+	for _, line := range happyRows {
+		w.FgColors[line] = termui.ColorDefault
+	}
 
 	return w
 }
