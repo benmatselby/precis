a867159b7ba67a52ddbd8757b3f7e498c0df100b
Ben Selby <benmatselby@gmail.com>
2018-04-28 10:41:11 +0100

    Add VSTS pull request information

--- vsts.go
+++ vsts.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/benmatselby/go-vsts/vsts"
@@ -8,7 +9,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func doVsts() (*termui.Table, error) {
+func getVstsBuilds() (*termui.Table, error) {
 	client := vsts.NewClient(vstsAccount, vstsProject, vstsToken)
 	builds, error := client.Builds.List()
 	if error != nil {
@@ -61,17 +62,70 @@ func doVsts() (*termui.Table, error) {
 	return w, nil
 }
 
+func getVstsPulls() (*termui.Table, error) {
+	client := vsts.NewClient(vstsAccount, vstsProject, vstsToken)
+	opts := vsts.PullRequestListOptions{State: "active"}
+	pulls, count, error := client.PullRequests.List(&opts)
+	if error != nil {
+		logrus.Fatal(error)
+	}
+
+	rows := [][]string{
+		{"id", "repo", "title", "created"},
+	}
+
+	for index := 0; index < count; index++ {
+		pull := pulls[index]
+
+		created, error := time.Parse(time.RFC3339, pull.Created)
+		createdOn := created.Format("2006-01-02 15:04:05")
+		if error != nil {
+			createdOn = pull.Created
+		}
+
+		rows = append(rows, []string{fmt.Sprintf("%d", pull.ID), pull.Repo.Name, pull.Title, createdOn})
+	}
+
+	if count == 0 {
+		rows = append(rows, []string{"No", "open", "pull", "requests"})
+	}
+
+	w := termui.NewTable()
+	w.Rows = rows
+	w.FgColor = termui.ColorWhite
+	w.BgColor = termui.ColorDefault
+	w.TextAlign = termui.AlignLeft
+	w.Border = true
+	w.Block.BorderLabel = "VSTS Pull Requests - " + vstsTeam
+
+	w.Analysis()
+	w.SetSize()
+
+	return w, nil
+}
+
 func vstsWidget(body *termui.Grid) {
 	if body == nil {
 		body = termui.Body
 	}
 
-	travis, err := doVsts()
+	builds, err := getVstsBuilds()
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	if travis != nil {
-		body.AddRows(termui.NewRow(termui.NewCol(5, 0, travis)))
+
+	pulls, err := getVstsPulls()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	if builds != nil {
+		body.AddRows(
+			termui.NewRow(
+				termui.NewCol(5, 0, builds),
+				termui.NewCol(7, 0, pulls),
+			),
+		)
 
 		// Calculate the layout.
 		body.Align()
